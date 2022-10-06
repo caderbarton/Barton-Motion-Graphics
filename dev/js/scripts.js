d147c6f11e8379ee02fc7799dc597b73fc3bce26
caderbarton <crb389@drexel.edu>
2022-10-06 12:28:30 -0400

    a

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,7 +4,6 @@ gsap.from("#h1",{duration:1,y:-200, alpha:0});
 gsap.from("#h2",{duration:1,y:-100, alpha:0, delay: 1});
 gsap.from("#button",{duration:1,y:200, alpha:0, delay: 1});
 
-
 let reservation_button = document.querySelector("#button");
 reservation_button.addEventListener("mouseover", function(){
     gsap.from("#button",{duration:1,scaleX:1})
