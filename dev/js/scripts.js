dbf7974fd7c736845aa7751393065e622e003782
caderbarton <crb389@drexel.edu>
2022-10-27 13:48:50 -0400

    a

---
+++ dev/js/scripts.js
@@ -0,0 +1,36 @@
+import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+
+
+
+function simpleMotion(){
+    var tl = gsap.timeline()
+
+    return tl;
+}
+
+function patternMotion(){
+    var tl = gsap.timeline()
+    tl.from(".odd",{duration:0.5,rotate:180,drawSVG:0,stagger:0.25,transformOrigin:"center"});
+
+    return tl;
+}
+
+function UIMotion(){
+    var tl = gsap.timeline()
+
+    return tl;
+}
+
+
+var mainTL = gsap.timeline();
+mainTL.add(simpleMotion())
+    .add(patternMotion())
+    .add(UIMotion());
+
+
+
+GSDevTools.create();
