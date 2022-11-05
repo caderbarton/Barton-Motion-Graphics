42c175ddec067ec239a854dc295939f0485109b1
caderbarton <crb389@drexel.edu>
2022-11-05 09:59:40 -0400

    updates

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -9,13 +9,13 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 function simpleMotion(){
     var tl = gsap.timeline()
     tl.from("#ball3",{scale:.25, transformOrigin:"center"})
-    tl.from("#Vector2",{drawSVG:0, duration: .5})
+    tl.from("#Vector2",{drawSVG:0, duration: .25})
     tl.from("#ball1",{scale:.25, transformOrigin:"center"})
-    tl.from("#Vector3",{drawSVG:0, duration: .5})
+    tl.from("#Vector3",{drawSVG:0, duration: .25})
     tl.from("#ball2",{scale:.25, transformOrigin:"center"})
-    tl.from("#Vector1",{drawSVG:0, duration: .5})
+    tl.from("#Vector1",{drawSVG:0, duration: .25})
     tl.to(".circle",{scale:.25, transformOrigin:"center"})
-    tl.to(".line",{drawSVG:0, duration: .5})
+    tl.to(".line",{drawSVG:0, duration: .25})
 
 
     return tl;  
@@ -34,12 +34,12 @@ function patternMotion(){
 function UIMotion(){  
 
     var tl = gsap.timeline()
-    tl.from(".disk",{alpha:0,stagger:.25})
-    tl.to("#black",{drawSVG:0 , duration: 1,  ease: "bounce.out"})
-    tl.to("#red",{drawSVG: 30, duration: 1, ease: "bounce.out"})
-    tl.to("#orange",{drawSVG:60, duration: 1,  ease: "bounce.out"})
-    tl.to("#yellow",{drawSVG:90, duration: 1,  ease: "bounce.out"})
-    tl.to("#green",{drawSVG:120, duration: 1,  ease: "bounce.out"})
+    tl.from(".disk",{alpha:0,stagger:.15})
+    tl.to("#black",{drawSVG:0 , duration: 1,  stagger:.25, ease: "bounce.out"})
+    tl.to("#red",{drawSVG: 20, duration: 1, stagger:.25, ease: "bounce.out"})
+    tl.to("#orange",{drawSVG:60, duration: 1,  stagger:.25, ease: "bounce.out"})
+    tl.to("#yellow",{drawSVG:120, duration: 1,  stagger:.25, ease: "bounce.out"})
+    tl.to("#green",{drawSVG:200, duration: 1,  stagger:.25, ease: "bounce.out"})
     tl.to(".disk",{alpha:0,stagger:.05})
 
 } 
