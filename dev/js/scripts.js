bf862bb6dd41c43162f7bf3cb885a17927a22bad
caderbarton <crb389@drexel.edu>
2022-12-01 11:39:26 -0500

    a

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,13 +4,19 @@ import { GSDevTools } from "gsap/GSDevTools";
 
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
-
+function crystal(){
+    var tl = gsap.timeline()
+    tl.from("#crys",{duration:5, alpha:0, stagger:.5})
+    tl.to("#lines",{drawSVG:"0"})
+    tl.from("#sun",{clipPath:"0 100 0"})
+}
  
 
  
 
 
 
-
+var mainTL=gsap.timeline();
+mainTL.add(crystal())
 
 GSDevTools.create();
